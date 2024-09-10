21c4ce2db0e365808157045d6e6f9602b3d142a3
newways <reactwebdew@gmail.com>
2024-09-10 21:41:13 +0200

    workspace sidebar

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -3,6 +3,12 @@
 import { ReactNode } from 'react'
 import { Toolbar } from './toolbar'
 import { Sidebar } from './sidebar'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
+import { WorkspaceSidebar } from './workspace-sidebar'
 
 interface WorkspaceIdLayoutProps {
   children: ReactNode
@@ -14,7 +20,20 @@ const WorkspaceLayout = ({ children }: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className='flex h-[calc(100vh-40px)]'>
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction='horizontal'
+          autoSaveId='kd-workspace-layout'
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className='bg-[#5e2c5f]'
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   )
---
+++ src/components/hint.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { ReactNode } from 'react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './ui/tooltip'
+
+interface HintProps {
+  label: string
+  children: ReactNode
+  side?: 'top' | 'right' | 'bottom' | 'left'
+  align?: 'start' | 'center' | 'end'
+}
+
+export const Hint = ({ label, children, side, align }: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          side={side}
+          align={align}
+          className='bg-black text-white border border-white/5'
+        >
+          <p className='font-medium text-xs'>{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
